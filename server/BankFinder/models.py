ef5c224ff72486716a728126e61273f8b1301ce4
siriuzz <elianmatosd03@gmail.com>
2023-12-10 01:33:37 -0400

    modelos de base de datos creados

---
+++ server/BankFinder/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+
+class bank(models.Model):
+    bank_id=models.IntegerField(primary_key=True, unique=True,null=False)
+    bank_name = models.CharField(max_length=250, null=False, default="-")
+    website = models.CharField(max_length=250,null=False, default="N/A")
+    contact_number = models.CharField(max_length=15,null=False, default="000-000-0000")
+    pass
+
+class branch(models.Model):
+    bank_id=models.ForeignKey(bank, on_delete=models.CASCADE,db_column='bank_id')
+    branch_id = models.IntegerField(primary_key=True, unique=True)
+    branch_name = models.CharField(max_length=200, null=False)
+    location = models.CharField(max_length=200, null=False)
+    branch_contact_number = models.CharField(max_length=15, null=False)
+    opening_hour = models.TimeField(null=False)
+    closing_hour = models.TimeField(null=False)
+
+class source_currency(models.Model):
+    source_currency_id = models.IntegerField(primary_key=True,unique=True,null=False)
+    currency_code = models.CharField(max_length=3,null=False)
+    currency_name = models.CharField(max_length=150,null=False)
+    pass
+    
+class target_currency(models.Model):
+    target_currency_id = models.IntegerField(primary_key=True,unique=True,null=False)
+    currency_code = models.CharField(max_length=3,null=False)
+    currency_name = models.CharField(max_length=150,null=False)
+    pass
+
+class exchange_rate(models.Model):
+    exchange_rate_id = models.IntegerField(primary_key=True, unique=True,null=False)
+    source_currency_id = models.ForeignKey(source_currency, on_delete=models.CASCADE,null=False, db_column='source_currency_id')
+    target_currency_id = models.ForeignKey(target_currency, on_delete=models.CASCADE,null=False, db_column='target_currency_id')
+    last_update = models.DateTimeField(null=False)
+    def __str__(self):
+        return self
+    pass
+
+class bank_exchange_rate(models.Model):
+    bank_id=models.ForeignKey(bank, on_delete=models.CASCADE, db_column='bank_id')
+    exchange_rate_id = models.ForeignKey(exchange_rate,primary_key=True, on_delete=models.CASCADE, db_column='exchange_rate_id')
+    rate = models.FloatField(null=False)
+    last_update = models.DateTimeField(null=False)
+
+    
+    
+
+
+   
