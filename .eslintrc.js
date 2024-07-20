34eb1984a5be10d6a835d8eb955af4ef039d71ae
siriuzz <elianmatosd03@gmail.com>
2024-07-20 15:46:43 -0400

    linter configurado

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,4 +7,11 @@ module.exports = {
     'plugin:vue/vue3-essential',
     'eslint:recommended',
   ],
-}
+  ignorePatterns: ["/local"], // Ignore everything except the src folder
+  overrides: [
+    {
+      files: ['src/**/*.js'], // Apply ESLint rules only to .js files in the src folder
+    },
+  ],
+};
+
